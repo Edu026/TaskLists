51964b8f42e8b2c45d67eec8131411bc1c257fc1
«@Edu026» <edu.amoros26@gmail.com>
2024-01-17 20:57:00 +0100

    localStorage

--- www/js/index.js
+++ www/js/index.js
@@ -1,35 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 
-    // !!!!
-    //document.getElementById('deviceready').classList.add('ready');
-
     init();
+    loadListFromLocalStorage(); // Cargar la lista almacenada al iniciar la aplicación
 }
 
 function init() {
@@ -37,14 +12,37 @@ function init() {
 }
 
 function addTask() {
-    var newelem = $("<li>new element <button>edit</button> <button class='removeTask'>X</button></li>");
-    $("ul").append( newelem );
+    var text = window.prompt("TASK NAME?");
+    var newelem = $("<li>" + text + "<button>edit</button> <button class='removeTask'>X</button></li>");
+    $("ul").append(newelem);
     $("ul").listview("refresh");
     $(".removeTask").click(removeTask);
+
+    saveListToLocalStorage(); // Guardar la lista en el localStorage después de agregar un elemento
 }
 
-function removeTask (e)
-{
+function removeTask(e) {
     var caller = e.target;
     $(caller).parent().remove();
+    saveListToLocalStorage(); // Guardar la lista en el localStorage después de eliminar un elemento
+}
+
+function editTask(e){
+    var caller = e.target;
+    
+}
+
+function saveListToLocalStorage() {
+    // Obtener la lista actual y guardarla en el localStorage
+    var taskList = $("ul").html();
+    localStorage.setItem('taskList', taskList);
+}
+
+function loadListFromLocalStorage() {
+    // Cargar la lista almacenada en el localStorage
+    var storedList = localStorage.getItem('taskList');
+    if (storedList) {
+        $("ul").html(storedList);
+        $(".removeTask").click(removeTask); // Volver a vincular el evento después de cargar la lista
+    }
 }
